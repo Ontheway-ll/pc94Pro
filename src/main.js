cbe69b8f15aafbcd2fe164203a3640c6d6989be7
liu lei <1206821643@qq.com>
2020-03-02 21:07:06 +0800

    axios配置和全局使用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import router from './router'
 import ElementUI from 'element-ui' // 全局注册UI，所有位置都可以使用
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.less'// 引入样式
-Vue.use(ElementUI) // 引入模块
+import axios from 'axios'// 引入axios
+Vue.use(ElementUI) // 使用模块
+// 全局注册element-ui框架 一旦全局注册 所有的位置都可以使用组件
+// Vue.use实际上 会调用 ElementUI对象里的一个方法 install方法,并且传入当前的Vue对象
+// 配置axios的baseURL
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求头地址
+// 将axios赋值给vue对象的原型对象
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 new Vue({
   router,
