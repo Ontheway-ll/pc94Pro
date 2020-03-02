298fc924a7e29589225a0cda9e230eb6b20d162e
liu lei <1206821643@qq.com>
2020-03-02 23:28:04 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,23 @@
 <template>
-<div>
-<h1>我是主页</h1>
-</div>
+<!-- 在最外层放一个大容器 -->
+<el-container>
+     <el-aside>
+         <h1>左侧导航区域</h1>
+     </el-aside>
+     <!-- 右侧嵌套一个容器 -->
+     <el-container>
+         <!-- 上下布局 -->
+         <el-header>
+             <h1>顶部操作区域</h1>
+         </el-header>
+         <!-- 主要区域 -->
+         <el-main>
+             <h1>主要区域</h1>
+             <!-- 这里放置二级路由容器 -->
+         <router-view></router-view>
+         </el-main>
+     </el-container>
+</el-container>
 </template>
 
 <script>
