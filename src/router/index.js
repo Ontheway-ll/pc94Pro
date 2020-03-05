7c42ce1b3b195695bec14405d3f60d78599be72c
liu lei <1206821643@qq.com>
2020-03-05 22:47:27 +0800

    评论列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
       { // 一级路由表children下面配置二级路由表
         path: '', // 二级路由的path 什么都不写 代表二级路由的默认组件 如果 a => /home/a 如果什么都不写  /home
         component: SecondHome // 默认的二级路由组件
+      },
+      {
+        // path:'/home/comment'//完整写法
+        path: 'comment',
+        component: () => import('@/views/comment')// 按需加载
       }
     ]
   }, {
