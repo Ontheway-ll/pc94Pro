0e08b3baa578cf76487950e6e325225c035194b7
liu lei <1206821643@qq.com>
2020-03-05 22:11:33 +0800

    面包屑封装

---
+++ src/components/common/breadcrumb.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 封装一个面包屑 -->
+  <el-breadcrumb separator=">">
+      <!-- el-breadcrumb-item 相当于 router-link标签 -->
+ <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+ <!-- 第二个选项是动态的，需要传值的方式传进来1是props二是slot插槽， -->
+ <el-breadcrumb-item>
+     <!-- 定义一个具名插槽 ,第一步挖坑，-->
+     <!-- 需要注册 -->
+     <slot name="title"></slot>
+ </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@
 // Vue.use会调用对象中的install方法 install方法第一个参数 是Vue对象
 import LayoutAside from './home/layout-aside'// 引入
 import LayoutHeader from './home/layout-header'
+import BreadCrumb from './common/breadcrumb.vue'// 引入面包屑组件
 export default {
   install: function (Vue) {
     //   注册全局组件 Vue 一旦注册 在任意位置都可以使用
     Vue.component('layout-aside', LayoutAside)
-    Vue.component('layout-header', LayoutHeader)
+    Vue.component('layout-header', LayoutHeader)// 全局注册
+    Vue.component('bread-crumb', BreadCrumb)// 全局注册面包屑组件
   }
 }
