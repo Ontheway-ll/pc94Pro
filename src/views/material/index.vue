a829bebe9c0a3185a733584d46075c5077243d1d
liu lei <1206821643@qq.com>
2020-03-08 20:29:59 +0800

    -素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -7,12 +7,29 @@
           素材管理
       </template>
       </bread-crumb>
+      <!-- 放置标签页e-tabs -->
+      <!-- v-model所绑定的值，就是当前激活的页签 -->
+      <el-tabs v-model="activeName">
+        <!-- lable显示标签内容 name表示显示内容的值 -->
+        <el-tab-pane label="全部素材" name="all">
+          <!-- 内容 -->
+          全部
+        </el-tab-pane>
+        <el-tab-pane label="收藏素材" name="collect">
+          <!-- 内容 -->
+          收藏
+        </el-tab-pane>
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'// 默认选择
+    }
+  }
 }
 </script>
 
